952ebdeccd56b833b8e347a25761819b1bee866b
unknown <arturisisr_07@hotmail.com>
2023-04-12 17:40:58 -0600

    agregando color al fondo2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
-import styles from './FeedbackForm.module.css'
+import styles from '@components/FeedbackForm.module.css'
 
 export default function Home() {
   return (
