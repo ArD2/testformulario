cba765f857d873cf6cf373a4c2c6809ccab35b9d
unknown <arturisisr_07@hotmail.com>
2023-04-12 19:36:23 -0600

    agregando color al fondo6

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,27 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
 import JokeBlock from "@components/JokeBlock";
-import styles from '@components/FeedbackForm.module.css'
+import styles from "@components/FeedbackForm.module.css";
 
 export default function Home() {
   return (
+    <div className="container">
+      <div className={styles["background-colors"]}>
+        <Head>
+          <title>Cuestionario Prueba</title>
+          <link rel="icon" href="/ticoman.ico" />
+        </Head>
 
-    <div className="container teal">
-      <Head>
-        <title>Cuestionario Prueba</title>
-        <link rel="icon" href="/ticoman.ico" />
-      </Head>
-
-      <main>
-        <Header title="Cuestionario IPN ESIME Ticomán" />
-        <hr />
-        <p className="description">
-          Favor de llenar el custionario con sus respectivas respuestas:
-        </p>
-        <FeedbackForm />
-      </main>
-      <Footer />
+        <main>
+          <Header title="Cuestionario IPN ESIME Ticomán" />
+          <hr />
+          <p className="description">
+            Favor de llenar el custionario con sus respectivas respuestas:
+          </p>
+          <FeedbackForm />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
